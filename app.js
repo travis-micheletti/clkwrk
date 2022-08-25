8f357f6415b20a345fc35d3a83133a88d4345e78
Travis Micheletti <106038655+travis-micheletti@users.noreply.github.com>
2022-08-24 17:41:30 -0700

    will try and handle auth on the front end since i was having issues redirecting back to my frontend from backend auth

--- app.js
+++ app.js
@@ -4,20 +4,20 @@
 const express = require('express')
 const app = express()
 // const cors = require('cors')
-const { auth, requiresAuth } = require('express-openid-connect')
+// const { auth, requiresAuth } = require('express-openid-connect')
 require('dotenv').config()
 require('./db/connection')
 
 
 // config variable for auth
-const config = {
-    authRequired: false,
-    auth0Logout: true,
-    secret: process.env.SECRET,
-    baseURL: process.env.BASE_URL,
-    clientID: process.env.CLIENT_ID,
-    issuerBaseURL: process.env.ISSUER_BASE_URL
-  };
+// const config = {
+//     authRequired: false,
+//     auth0Logout: true,
+//     secret: process.env.SECRET,
+//     baseURL: process.env.BASE_URL,
+//     clientID: process.env.CLIENT_ID,
+//     issuerBaseURL: process.env.ISSUER_BASE_URL
+//   };
 
 const port = process.env.PORT || 8000
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -30,7 +30,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
+// app.use(auth(config));
 
 // app.use(cors())
 
@@ -39,15 +39,15 @@ app.use(auth(config));
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-});
+// app.get('/', (req, res) => {
+//   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+// });
 
-// basic profile route for logged-in user - > change to home screen
-app.get('/home', requiresAuth(), (req, res) => {
-    res.send(JSON.stringify(req.oidc.user))
-    console.log(req.oidc.user)
-})
+// // basic profile route for logged-in user - > change to home screen
+// app.get('/home', requiresAuth(), (req, res) => {
+//     res.send(JSON.stringify(req.oidc.user))
+//     console.log(req.oidc.user)
+// })
 
 // Controllers
 
